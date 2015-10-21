0d783532807e27c29b6889455c1d4acfbea8296c
liwangwang123 <2317790024@qq.com>
2015-10-21 10:05:50 +0800

    Initial Commit

---
+++ Wang/Wang/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Wang
+//
+//  Created by 王力 on 15/10/21.
+//  Copyright (c) 2015年 王力. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
